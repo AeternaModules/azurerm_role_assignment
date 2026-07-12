e28538e86f9a046f188e9b72bdae8a87daaf1b20
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,45 @@
-output "role_assignments" {
-  description = "All role_assignment resources"
-  value       = azurerm_role_assignment.role_assignments
-}
 output "role_assignments_condition" {
-  description = "List of condition values across all role_assignments"
-  value       = [for k, v in azurerm_role_assignment.role_assignments : v.condition]
+  description = "Map of condition values across all role_assignments, keyed the same as var.role_assignments"
+  value       = { for k, v in azurerm_role_assignment.role_assignments : k => v.condition }
 }
 output "role_assignments_condition_version" {
-  description = "List of condition_version values across all role_assignments"
-  value       = [for k, v in azurerm_role_assignment.role_assignments : v.condition_version]
+  description = "Map of condition_version values across all role_assignments, keyed the same as var.role_assignments"
+  value       = { for k, v in azurerm_role_assignment.role_assignments : k => v.condition_version }
 }
 output "role_assignments_delegated_managed_identity_resource_id" {
-  description = "List of delegated_managed_identity_resource_id values across all role_assignments"
-  value       = [for k, v in azurerm_role_assignment.role_assignments : v.delegated_managed_identity_resource_id]
+  description = "Map of delegated_managed_identity_resource_id values across all role_assignments, keyed the same as var.role_assignments"
+  value       = { for k, v in azurerm_role_assignment.role_assignments : k => v.delegated_managed_identity_resource_id }
 }
 output "role_assignments_description" {
-  description = "List of description values across all role_assignments"
-  value       = [for k, v in azurerm_role_assignment.role_assignments : v.description]
+  description = "Map of description values across all role_assignments, keyed the same as var.role_assignments"
+  value       = { for k, v in azurerm_role_assignment.role_assignments : k => v.description }
 }
 output "role_assignments_name" {
-  description = "List of name values across all role_assignments"
-  value       = [for k, v in azurerm_role_assignment.role_assignments : v.name]
+  description = "Map of name values across all role_assignments, keyed the same as var.role_assignments"
+  value       = { for k, v in azurerm_role_assignment.role_assignments : k => v.name }
 }
 output "role_assignments_principal_id" {
-  description = "List of principal_id values across all role_assignments"
-  value       = [for k, v in azurerm_role_assignment.role_assignments : v.principal_id]
+  description = "Map of principal_id values across all role_assignments, keyed the same as var.role_assignments"
+  value       = { for k, v in azurerm_role_assignment.role_assignments : k => v.principal_id }
 }
 output "role_assignments_principal_type" {
-  description = "List of principal_type values across all role_assignments"
-  value       = [for k, v in azurerm_role_assignment.role_assignments : v.principal_type]
+  description = "Map of principal_type values across all role_assignments, keyed the same as var.role_assignments"
+  value       = { for k, v in azurerm_role_assignment.role_assignments : k => v.principal_type }
 }
 output "role_assignments_role_definition_id" {
-  description = "List of role_definition_id values across all role_assignments"
-  value       = [for k, v in azurerm_role_assignment.role_assignments : v.role_definition_id]
+  description = "Map of role_definition_id values across all role_assignments, keyed the same as var.role_assignments"
+  value       = { for k, v in azurerm_role_assignment.role_assignments : k => v.role_definition_id }
 }
 output "role_assignments_role_definition_name" {
-  description = "List of role_definition_name values across all role_assignments"
-  value       = [for k, v in azurerm_role_assignment.role_assignments : v.role_definition_name]
+  description = "Map of role_definition_name values across all role_assignments, keyed the same as var.role_assignments"
+  value       = { for k, v in azurerm_role_assignment.role_assignments : k => v.role_definition_name }
 }
 output "role_assignments_scope" {
-  description = "List of scope values across all role_assignments"
-  value       = [for k, v in azurerm_role_assignment.role_assignments : v.scope]
+  description = "Map of scope values across all role_assignments, keyed the same as var.role_assignments"
+  value       = { for k, v in azurerm_role_assignment.role_assignments : k => v.scope }
 }
 output "role_assignments_skip_service_principal_aad_check" {
-  description = "List of skip_service_principal_aad_check values across all role_assignments"
-  value       = [for k, v in azurerm_role_assignment.role_assignments : v.skip_service_principal_aad_check]
+  description = "Map of skip_service_principal_aad_check values across all role_assignments, keyed the same as var.role_assignments"
+  value       = { for k, v in azurerm_role_assignment.role_assignments : k => v.skip_service_principal_aad_check }
 }
 
